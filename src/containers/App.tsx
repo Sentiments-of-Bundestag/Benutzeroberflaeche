a74220598bba122248c4a0a0ced1664ae6328d2b
abteilung6 <moeller.tilman@gmail.com>
2020-12-02 13:04:36 +0100

    load user selector in app

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'react-bootstrap';
 
 import { getUsersStart } from '../slices/users';
+import { getAllUsers } from '../selectors/users';
 import { Header } from '../components/Header';
 import { Layout } from '../components/Layout';
 
 const App: React.FC = () => {
   const dispatch = useDispatch();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { users, isLoading, error } = useSelector(getAllUsers);
+
+  useEffect(() => {
+    dispatch(getUsersStart());
+  }, [dispatch]);
 
   const onLoadUsersClick = () => {
     dispatch(getUsersStart());
@@ -17,7 +25,10 @@ const App: React.FC = () => {
     <>
       <Header links={[{ name: 'Home', route: '/' }]} />
       <Layout>
-        <Button onClick={onLoadUsersClick}>Button</Button>
+        <Button onClick={onLoadUsersClick}>Reload</Button>
+        {users.map((user, index) => (
+          <p key={index.toString()}>{user.name}</p>
+        ))}
       </Layout>
     </>
   );
