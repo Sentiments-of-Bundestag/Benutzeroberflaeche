e46003a15078d8462701ef25c28fdc3d79af0cbf
abteilung6 <moeller.tilman@gmail.com>
2020-12-02 11:50:18 +0100

    add provider with store

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
+import store from './store';
 import { Routes } from './routes';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 const render = () => {
   ReactDOM.render(
-    <Router>
-      <Routes />
-    </Router>,
+    <Provider store={store}>
+      <Router>
+        <Routes />
+      </Router>
+    </Provider>,
     document.getElementById('root'),
   );
 };
