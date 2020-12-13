730bc04859018aab58acc5b6ec7ec5a0c4f8a14f
abteilung6 <moeller.tilman@gmail.com>
2020-12-13 13:24:10 +0100

    add person reducer to root reducer

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -2,10 +2,12 @@ import { combineReducers } from '@reduxjs/toolkit';
 
 import userReducer from './slices/users';
 import factionReducer from './slices/factions';
+import personReducer from './slices/person';
 
 const combinedReducers = combineReducers({
   users: userReducer,
   factions: factionReducer,
+  persons: personReducer,
 });
 
 export default combinedReducers;
