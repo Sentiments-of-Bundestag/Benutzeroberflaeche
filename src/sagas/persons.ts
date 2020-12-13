b3a57c7cb0a9b10ca135d08c914251d2f8562181
abteilung6 <moeller.tilman@gmail.com>
2020-12-13 14:24:21 +0100

    handle personsGraph side effect with faction saga

--- src/sagas/persons.ts
+++ src/sagas/persons.ts
@@ -1,17 +1,24 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
 
-import { Person } from '../types';
+import { Person, PersonGraph } from '../types';
 
 import { request } from '../utils/client';
 import {
   getPersonsStart,
   getPersonsSuccess,
   getPersonsFailed,
+  getPersonGraphsStart,
+  getPersonGraphsSuccess,
+  getPersonGraphsFailed,
 } from '../slices/persons';
 
-const { REACT_APP_ENDPOINT_PERSONS } = process.env;
+const {
+  REACT_APP_ENDPOINT_PERSONS,
+  REACT_APP_ENDPOINT_PERSON_GRAPH,
+} = process.env;
 
 const apiPersonsUrls = REACT_APP_ENDPOINT_PERSONS || '';
+const apiPersonGraphsUrls = REACT_APP_ENDPOINT_PERSON_GRAPH || '';
 
 function* handleGetPersons() {
   try {
@@ -22,6 +29,21 @@ function* handleGetPersons() {
   }
 }
 
+function* handleGetPersonGraphs() {
+  try {
+    const response = yield call(request, apiPersonGraphsUrls);
+    if (Object.prototype.hasOwnProperty.call(response, 'messages')) {
+      const personGraphs: PersonGraph[] = response.messages;
+      yield put(getPersonGraphsSuccess(personGraphs));
+    } else {
+      yield put(getPersonGraphsFailed('Response has no property messages'));
+    }
+  } catch (error) {
+    yield put(getPersonGraphsFailed(error.toString()));
+  }
+}
+
 export function* personsSaga() {
   yield takeEvery(getPersonsStart.type, handleGetPersons);
+  yield takeEvery(getPersonGraphsStart.type, handleGetPersonGraphs);
 }
