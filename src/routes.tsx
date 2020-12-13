c702eaac16b0dfc334ce954be31ce473376cf8ee
abteilung6 <moeller.tilman@gmail.com>
2020-12-13 13:51:23 +0100

    add route to PersonsPage

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import FactionsPage from './containers/FactionsPage';
+import PersonsPage from './containers/PersonsPage';
 
 export interface RoutesProps {}
 
@@ -9,5 +10,6 @@ export const Routes: React.FC<RoutesProps> = () => (
   <Switch>
     <Route exact path="/" component={FactionsPage} />
     <Route exact path="/factions" component={FactionsPage} />
+    <Route exact path="/persons" component={PersonsPage} />
   </Switch>
 );
