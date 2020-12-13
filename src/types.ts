c9906322ac694d0bea8587f9efdd61d02f9b8429
abteilung6 <moeller.tilman@gmail.com>
2020-12-13 11:00:52 +0100

    add comments tu current types, incoming types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,10 @@
+// JSON Placeholder
 export interface UserInterface {
   id: number;
   name: string;
 }
 
+// React Router DOM
 export interface NavigationLinkInterface {
   name: string;
   route: string;
