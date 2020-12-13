3a85e78d1581c855d54d0862c21ceaaa7b2ee964
abteilung6 <moeller.tilman@gmail.com>
2020-12-13 11:27:12 +0100

    add factionReducer to rootReducer

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import userReducer from './slices/users';
+import factionReducer from './slices/factions';
 
 const combinedReducers = combineReducers({
   users: userReducer,
+  factions: factionReducer,
 });
 
 export default combinedReducers;
