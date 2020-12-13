cfb0f58e950d8e5df226e84c6006fac4c1dcc9d2
abteilung6 <moeller.tilman@gmail.com>
2020-12-13 14:27:10 +0100

    display personGraphs in PersonPage

--- src/containers/PersonsPage.tsx
+++ src/containers/PersonsPage.tsx
@@ -2,21 +2,29 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'react-bootstrap';
-import { getPersonsStart } from '../slices/persons';
+import { getPersonsStart, getPersonGraphsStart } from '../slices/persons';
 import { getAllPersons } from '../selectors/persons';
 import { Layout } from '../components/Layout';
 
 const PersonsPage: React.FC = () => {
   const dispatch = useDispatch();
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { persons, arePersonsLoading, error } = useSelector(getAllPersons);
+  const {
+    persons,
+    arePersonsLoading,
+    personGraphs,
+    arePersonGraphsLoading,
+    error,
+  } = useSelector(getAllPersons);
 
   useEffect(() => {
     dispatch(getPersonsStart());
+    dispatch(getPersonGraphsStart());
   }, [dispatch]);
 
   const onLoadPersonsClick = () => {
     dispatch(getPersonsStart());
+    dispatch(getPersonGraphsStart());
   };
 
   return (
@@ -25,6 +33,11 @@ const PersonsPage: React.FC = () => {
         <Button onClick={onLoadPersonsClick}>Reload</Button>
         {persons.map((person, index) => (
           <p key={index.toString()}>{`${person.name} ranked ${person.rank}`}</p>
+        ))}
+        {personGraphs.map((personGraph, index) => (
+          <p key={index.toString()}>
+            {`${personGraph.sender} to ${personGraph.recipient} with ${personGraph.sentiment}`}
+          </p>
         ))}
       </Layout>
     </>
