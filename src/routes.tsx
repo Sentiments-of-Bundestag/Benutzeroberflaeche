d4d366298fee9d344d2b201d15e5dbaef6711303
abteilung6 <moeller.tilman@gmail.com>
2020-12-16 21:17:13 +0100

    add route faction details with id param

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import FactionsPage from './containers/FactionsPage';
+import FactionDetailPage from './containers/FactionDetailPage';
 import PersonsPage from './containers/PersonsPage';
 
 export interface RoutesProps {}
@@ -10,6 +11,7 @@ export const Routes: React.FC<RoutesProps> = () => (
   <Switch>
     <Route exact path="/" component={FactionsPage} />
     <Route exact path="/factions" component={FactionsPage} />
+    <Route path="/factions/:id" component={FactionDetailPage} />
     <Route exact path="/persons" component={PersonsPage} />
   </Switch>
 );
