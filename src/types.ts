679354993d79bcc8e540bfba5de4a677d86c8b47
abteilung6 <moeller.tilman@gmail.com>
2020-12-20 20:42:44 +0100

    update interfaces

--- src/types.ts
+++ src/types.ts
@@ -11,27 +11,78 @@ export interface NavigationLinkInterface {
 }
 
 // Sentiment
+
+// Completly Missing
+export interface Session {
+  sessionId: number;
+  legislativePeriod: number;
+  endDateTime: string;
+  startDateTime: string;
+}
+
 export interface Faction {
+  // Missing
+  factionId: string;
+  // Missing
+  sessionId: number;
   name: string;
   size: number;
 }
 
 export interface FactionGraph {
-  from: string;
+  recipient: string;
+  // Missing
+  sessionId: number;
+  sender: string;
   sentiment: number;
-  to: string;
+  // Missing
+  count: number;
+}
+
+export interface FactionRanked {
+  // Missing
+  factionId: string;
+  // Missing
+  sessionId: string;
+  name: string;
+  size: number;
+  rank: number;
 }
 
 export interface Person {
+  speakerId: string;
+  // Missing
+  sessionId: number;
   name: string;
   rank: number;
   role: string;
-  speakerId: string;
 }
 
+// Completly missing
+export interface PersonMessage {
+  // Missing
+  sessionId: number;
+  sender: string;
+  recipient: string;
+  sentiment: number;
+}
+
+// Completly missing
 export interface PersonGraph {
+  // missing
+  sessionId: number;
   sender: string;
   recipient: string;
   sentiment: number;
   count: number;
+}
+
+export interface PersonRanked {
+  speakerId: number;
+  // missing
+  factionId: string;
+  faction: string;
+  name: string;
+  role: string;
+  rank: number;
 }
