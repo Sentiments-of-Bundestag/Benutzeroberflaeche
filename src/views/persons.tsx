9e6874501fa7de76f99fcf449f21a8eae68eb2f5
abteilung6 <moeller.tilman@gmail.com>
2021-01-31 20:24:15 +0100

    implement person view

--- src/views/persons.tsx
+++ src/views/persons.tsx
@@ -1,13 +1,67 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Alert } from 'react-bootstrap';
+import { Skeleton } from 'antd';
+
+// Reducer Actions
+import {
+  getFactionGraphsStart,
+  getFactionProportionsStart,
+  getFactionRanksStart,
+  getFactionsStart,
+} from '../slices/factions';
+import {
+  getPersonGraphsStart,
+  getPersonMessagesStart,
+  getPersonRanksStart,
+} from '../slices/persons';
+import { getSessionsStart } from '../slices/sessions';
+
+// Selectors
+import { getAllFactions } from '../selectors/factions';
+import { getAllPersons } from '../selectors/persons';
 
 import AppLayout from '../layout/AppLayout';
+import PersonTable from '../components/PersonTable';
 
 interface PersonsProps {}
 
-const Persons: React.FC<PersonsProps> = () => (
-  <>
-    <AppLayout>persons</AppLayout>
-  </>
-);
+const Persons: React.FC<PersonsProps> = () => {
+  const dispatch = useDispatch();
+  const { factions } = useSelector(getAllFactions);
+  const { personRanks, personGraphs } = useSelector(getAllPersons);
+
+  useEffect(() => {
+    dispatch(getFactionsStart());
+    dispatch(getFactionGraphsStart());
+    dispatch(getFactionRanksStart());
+    dispatch(getSessionsStart());
+    dispatch(getPersonGraphsStart());
+    dispatch(getPersonMessagesStart());
+    dispatch(getPersonRanksStart());
+    dispatch(getFactionProportionsStart());
+  }, [dispatch]);
+  return (
+    <>
+      <AppLayout>
+        <h1>Sentiment Analyse für Abgeordnete</h1>
+        <Alert variant="warning">
+          <b> Wähle einen Abgeordneten aus, indem du auf die Zeile der Tabelle klickst..</b>
+        </Alert>
+
+        {factions.length && personRanks.length && personGraphs.length ? (
+          <PersonTable
+            factions={factions}
+            personsGraph={personGraphs}
+            personsRanked={personRanks}
+            persons={personRanks}
+          />
+        ) : (
+          <Skeleton active />
+        )}
+      </AppLayout>
+    </>
+  );
+};
 
 export default Persons;
